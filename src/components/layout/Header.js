9670c8ff1c4a9d9f84785a9465f7f9a488955e69
Somya Garg <91609206+SomyaGarg611@users.noreply.github.com>
2026-03-31 00:35:27 +0530

    Refactor Header component for cleaner imports

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleNav, closeNav } from '../../redux/slices/uiSlice';
-import { searchMedia } from '../../redux/slices/searchSlice';
+import { toggleNav, closeNav } from 'redux/slices/uiSlice';
+import { searchMedia } from 'redux/slices/searchSlice';
 
 const Header = () => {
   const dispatch = useDispatch();
